1b501b95562e21fdcbdb96e5fc99b792ca29faba
Natalia Selemeneva <106182140+Selemeneva@users.noreply.github.com>
2022-12-05 17:54:49 +0300

    last version

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+
+public class Cat implements Serializable {
+    private String cat;
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+}
+
